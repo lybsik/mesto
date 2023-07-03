df990eb0e2edb61e1a2c3a856e86faf70233fd0c
Morozova Luba <lybs@bk.ru>
2023-07-03 17:52:17 +0300

    fix: Исправить все ошибки после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,27 @@
-const profileEditButton = document.querySelector(".profile__edit-button"); /*Заводим постоянную переменную выбирая из всего документа кнопку редактирования профиля*/
-const popupEditProfile = document.querySelector(".popup_type_edit-profile"); /*Заводим постоянную переменную выбирая из всего документа попап редактирования профиля*/
-const buttonCloseEditProfile = popupEditProfile.querySelector(".popup__close-button"); /*Заводим постоянную переменную выбирая из выше найденного попапа кнопку закрытия*/
-const formElement = document.querySelector(".form"); /*Заводим постоянную переменную выбирая из всего документа форму редактирования профиля*/
-const popupNameInput = formElement.querySelector(".form__input_type_name"); /*Объявляем переменную (элемент формы) выбирая первый нужный инпут из этой формы*/
-const popupAboutInput = formElement.querySelector(".form__input_type_about"); /*Объявляем переменную (элемент формы) выбирая второй нужный инпут из этой формы*/
-const nameInput = document.querySelector(".profile__name"); /*Объявляем первую переменную ввода выбирая из всего документа нужный текст*/
-const aboutInput = document.querySelector(".profile__caption"); /*Объявляем вторую переменную ввода выбирая из всего документа нужный текст*/
+const profileEditButton = document.querySelector('.profile__edit-button'); /*Заводим постоянную переменную выбирая из всего документа кнопку редактирования профиля*/
+const popupEditProfile = document.querySelector('.popup'); /*Заводим постоянную переменную выбирая из всего документа попап редактирования профиля*/
+const buttonCloseEditProfile = popupEditProfile.querySelector('.popup__close-button'); /*Заводим постоянную переменную выбирая из выше найденного попапа кнопку закрытия*/
+const formElement = document.querySelector('.form'); /*Заводим постоянную переменную выбирая из всего документа форму редактирования профиля*/
+const popupNameInput = formElement.querySelector('.form__input_type_name'); /*Объявляем переменную (элемент формы) выбирая первый нужный инпут из этой формы*/
+const popupAboutInput = formElement.querySelector('.form__input_type_about'); /*Объявляем переменную (элемент формы) выбирая второй нужный инпут из этой формы*/
+const nameInput = document.querySelector('.profile__name'); /*Объявляем первую переменную ввода выбирая из всего документа нужный текст*/
+const aboutInput = document.querySelector('.profile__caption'); /*Объявляем вторую переменную ввода выбирая из всего документа нужный текст*/
 
 /*При открытии попапа*/
 
 function openPopupEdit() {
     /*Функция редактирования профиля*/
-
-    popupEditProfile.classList.add("popup_opened"); /*Добавляется модификатор открытия со свойством видимости*/
     popupNameInput.value = nameInput.textContent; /*В первый элемент формы даем значение равное первому тексту (у нас это имя) и говорим что это текст*/
     popupAboutInput.value = aboutInput.textContent; /*Во второй элемент формы даем значение равное второму тексту (профессия) и говорим что это текст*/
+    popupEditProfile.classList.add('popup_opened'); /*Добавляется модификатор открытия со свойством видимости*/
+    
 }
 
 function closePopupEdit() {
     /*Функция закрытия попапа*/
 
-    popupEditProfile.classList.remove("popup_opened"); /*Удаляется модификатор открытия, попап становится снова невидимым*/
+    popupEditProfile.classList.remove('popup_opened'); /*Удаляется модификатор открытия, попап становится снова невидимым*/
 }
-
-/*Отслеживаем события*/
-
-profileEditButton.addEventListener("click", openPopupEdit); /*кнопка редактирования слышит как при нажатии на нее запускается Функция редактирования профиля*/
-buttonCloseEditProfile.addEventListener("click", closePopupEdit); /*кнопка закрытия на попапе слышит как при нажатии на нее запускается Функция закрытия попапа*/
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -42,7 +37,9 @@ function handleFormSubmit(evt) {
     aboutInput.textContent = popupAboutInput.value;
     closePopupEdit();
 }
-
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener("submit", handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmit);
+/*Отслеживаем события*/
+profileEditButton.addEventListener('click', openPopupEdit); /*кнопка редактирования слышит как при нажатии на нее запускается Функция редактирования профиля*/
+buttonCloseEditProfile.addEventListener('click', closePopupEdit); /*кнопка закрытия на попапе слышит как при нажатии на нее запускается Функция закрытия попапа*/
