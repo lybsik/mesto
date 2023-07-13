32a9ac544906e503c40561ea75899fdb8bc8593b
Morozova Luba <lybs@bk.ru>
2023-07-13 14:53:15 +0300

    feat: Изменить проект в соотвествии с требованиями 5 спринта

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,99 @@
-const profileEditButton = document.querySelector('.profile__edit-button'); /*Заводим постоянную переменную выбирая из всего документа кнопку редактирования профиля*/
-const popupEditProfile = document.querySelector('.popup'); /*Заводим постоянную переменную выбирая из всего документа попап редактирования профиля*/
-const buttonCloseEditProfile = popupEditProfile.querySelector('.popup__close-button'); /*Заводим постоянную переменную выбирая из выше найденного попапа кнопку закрытия*/
+const popupEditProfile = document.querySelector('.popup_edit'); 
+const popupAddPlace = document.querySelector('.popup_place');
+const popupPhoto = document.querySelector('.popup-photo')
+
+const profileEditButton = document.querySelector('.profile__edit-button'); 
+const buttonCloseEditProfile = popupEditProfile.querySelector('.popup__close-button'); 
+
 const formElement = document.querySelector('.form'); /*Заводим постоянную переменную выбирая из всего документа форму редактирования профиля*/
 const popupNameInput = formElement.querySelector('.form__input_type_name'); /*Объявляем переменную (элемент формы) выбирая первый нужный инпут из этой формы*/
 const popupAboutInput = formElement.querySelector('.form__input_type_about'); /*Объявляем переменную (элемент формы) выбирая второй нужный инпут из этой формы*/
 const nameInput = document.querySelector('.profile__name'); /*Объявляем первую переменную ввода выбирая из всего документа нужный текст*/
 const aboutInput = document.querySelector('.profile__caption'); /*Объявляем вторую переменную ввода выбирая из всего документа нужный текст*/
 
-/*При открытии попапа*/
+const profileAddPlaceButton = document.querySelector('.profile__add-button'); /*Заводим постоянную переменную выбирая из всего документа кнопку добавления фото*/
+const buttonCloseAddPlace = popupAddPlace.querySelector('.popup__close-button'); /*Заводим постоянную переменную выбирая из выше найденного попапа кнопку закрытия*/
 
-function openPopupEdit() {
-    /*Функция редактирования профиля*/
-    popupNameInput.value = nameInput.textContent; /*В первый элемент формы даем значение равное первому тексту (у нас это имя) и говорим что это текст*/
+const formPlaceElement = document.querySelector('#form_place'); //заводим переменную для формы добавления новой карточки
+const titleInput = formPlaceElement.querySelector('.form__input_type_title'); /*Объявляем переменную (элемент формы) выбирая первый нужный инпут из этой формы*/
+const linkInput = formPlaceElement.querySelector('.form__input_type_link'); /*Объявляем переменную (элемент формы) выбирая второй нужный инпут из этой формы*/
+
+const closeButtonPhotoPopup = document.querySelector('.popup-photo__close-button'); //создаем переменную кнопки закрытия фото выбрав из документа класс кнопки этой
+let photo = popupPhoto.querySelector('.popup-photo__image'); //создаем переменную фотографии выбрав из ранее созданной переменной класс с картинкой
+let title = popupPhoto.querySelector('.popup-photo__title'); //создаем переменную заголовка выбрав из ранее созданной переменной класс с заголовком
+
+const cardsContainer = document.querySelector('.cards'); //создали переменную карточки выбрав из документа класс cards
+let cardTemplate = document.querySelector('#card').content; //создали переменную из template из которой нам нужен будет контент
+
+initialCards.forEach((item) => { //перебираем массив с переменной item
+    let card = createCard(item.name, item.link) //создаем переменную карточки внутри которй будет созданы переменные со значениями name и link как в нашем массиве
+    cardsContainer.prepend(card) //объявляем переменную карточки появляющуся ПЕРЕД всеми остальными из массива
+})
+
+//Функция открывания попапов
+function openPopup(popup) {
+    popup.classList.add('popup_opened'); /*Добавляется модификатор открытия со свойством видимости*/
+    popupNameInput.value = nameInput.textContent; /*В первый элемент формы даем значение равное первому тексту (у нас это имя) и говорим что это текст-Это для редактирования информации*/
     popupAboutInput.value = aboutInput.textContent; /*Во второй элемент формы даем значение равное второму тексту (профессия) и говорим что это текст*/
-    popupEditProfile.classList.add('popup_opened'); /*Добавляется модификатор открытия со свойством видимости*/
-    
+    titleInput.value = ''; //даем значение названия для добавления новой карточки
+    linkInput.value = ''; //даем значение ссылки для добавления новой карточки
+    titleInput.placeholder = 'Название'; //тут пишем что по умолчанию будет в поле прописано
+    linkInput.placeholder = 'Ссылка на картинку'; //тут то же самое
 }
 
-function closePopupEdit() {
-    /*Функция закрытия попапа*/
-
-    popupEditProfile.classList.remove('popup_opened'); /*Удаляется модификатор открытия, попап становится снова невидимым*/
+//функция закрывания попапов
+function closePopup(popup) {
+    popup.classList.remove('popup_opened'); /*Удаляется модификатор открытия, попап становится снова невидимым*/
 }
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+profileEditButton.addEventListener('click', () => {openPopup(popupEditProfile)}); /*кнопка редактирования слышит как при нажатии на нее запускается Функция редактирования профиля*/
+buttonCloseEditProfile.addEventListener('click',() => {closePopup(popupEditProfile)}); /*кнопка закрытия на попапе слышит как при нажатии на нее запускается Функция закрытия попапа*/
+
+profileAddPlaceButton.addEventListener('click', () => {openPopup(popupAddPlace)});//кнопка с плюсиком слышит как при клике на нее запускается функция открытия попапа
+buttonCloseAddPlace.addEventListener('click', () => {closePopup(popupAddPlace)}); //кнопка с плюсиком слышит как при клике на нее запускается функция закрытия попапа
+
+closeButtonPhotoPopup.addEventListener('click', () => {closePopup(popupPhoto)}) //кнопка крестика на фото слышит как при клике на нее попап закрывается
+
+//функция Сохранения информации при редактировании профиля
 function handleFormSubmit(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    // Выберите элементы, куда должны быть вставлены значения полей
-    // Вставьте новые значения с помощью textContent
+    evt.preventDefault(); 
     nameInput.textContent = popupNameInput.value;
     aboutInput.textContent = popupAboutInput.value;
-    closePopupEdit();
+    closePopup(popupEditProfile);
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
-/*Отслеживаем события*/
-profileEditButton.addEventListener('click', openPopupEdit); /*кнопка редактирования слышит как при нажатии на нее запускается Функция редактирования профиля*/
-buttonCloseEditProfile.addEventListener('click', closePopupEdit); /*кнопка закрытия на попапе слышит как при нажатии на нее запускается Функция закрытия попапа*/
+
+formElement.addEventListener('submit', handleFormSubmit); //наша форма для редактирования слышит как при сохранении запускается функция сохранения информации при редактировании профиля
+
+//функция создания новой карточки
+function createCard (name, link) { //написали функцию с двумя переменными имя и ссылки (как в нашем массиве)
+    const card = cardTemplate.querySelector('.cards__list-item').cloneNode(true); //создали переменную выбрав из ранее созданной переменной нужный нам класс и клонируем его содержимое
+    const likeButton = card.querySelector('.cards__like-button'); //создали переменную лайка
+    const deleteButton = card.querySelector('.cards__delete-button'); //создали переменную кнопки удаления
+    const cardImage = card.querySelector('.cards__image'); //создали переменную изображения карточки
+    cardImage.src =  link //говоорим что искать мы ее будем из ссылки
+    const cardTitle = card.querySelector('.cards__title'); //создали переменную заголовка карточки
+    cardTitle.textContent = name; //сказали что текст для нее будем брать из имени
+    likeButton.addEventListener('click', function() { //кнопка лайка слушает как при клике на нее запускается функция:
+        likeButton.classList.toggle('cards__like-button_active'); //в которой переключается состояние нашего сердечка на активное
+    });
+    deleteButton.addEventListener('click', function() { //кнопка удаления прик клике запускает функцию:...
+        let element = deleteButton.closest('.cards__list-item'); //создадим переменную при которой корзина наша с классом
+        element.remove(); //...при которой наша переменная с карточкой удаляется
+    });
+    cardImage.addEventListener('click', function(){  //картинка слушает при клике вызывается функция, при которой:
+        popupPhoto.classList.add('popup_opened'); //добавляется класс открывашки
+        photo.src = cardImage.src; //фото ищется в картимэдже
+        title.textContent = cardTitle.textContent; //заголовок ищется в заголовке
+    })
+    return(card); //возвращаем готовую карточку со всем что выше мы в нее положили
+}
+//функция сохранения новой карточки
+function submitCard(evt) { 
+    evt.preventDefault();
+    let card = createCard(titleInput.value, linkInput.value);
+    cardsContainer.prepend(card);
+    closePopup(popupAddPlace);
+  }  
+  
+  formPlaceElement.addEventListener('submit', submitCard); //наша форма создания новой краточки слышит как при сохранении запускается функция созданная выше
+  
