9e35e4c2d2055ceda4d107cae96a8863ccb1e51b
Morozova Luba <lybs@bk.ru>
2023-07-13 14:59:18 +0300

    style: поменять картинки в массиве карточек

--- scripts/initial-cards.js
+++ scripts/initial-cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
   {
-    name: 'Кировск',
-    link: 'https://1.downloader.disk.yandex.ru/preview/90deedadc2081a2953e1242a74738df1ac75980186dee0b280a72ee24696b675/inf/qyZOduxJyOGZs37VvfZqMKaawkvVn23oTPirbFVVQ7Fws7IejAPN-gBxlHOURRag7vOt4WC5yoxyfx49l-Yp5w%3D%3D?uid=1648264466&filename=kirovsk.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1648264466&tknv=v2&size=2560x1232'
-  },
-  {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-    name: 'Шерегеш',
-    link: 'https://3.downloader.disk.yandex.ru/preview/6d94a800b0f0497e691bebec73d8594edd75c7127d5e39d431df1f1ccde7d3b2/inf/7EW4rTdsl0SvjbCNp7fEauEbIOlotRaOEQavkx5U5lzddGbzeNRhcAo-5UEdhlBKaeilqS9BmRvH2U14QHh5tg%3D%3D?uid=1648264466&filename=%D1%88%D0%B5%D1%80%D0%B5%D0%B3%D0%B5%D1%88.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1648264466&tknv=v2&size=2560x1232'
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
     name: 'Камчатка',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-    name: 'Домбай',
-    link: 'https://3.downloader.disk.yandex.ru/preview/8683920ea4b50d9e1191f576f63f0839ed8ad6788062fd3fb646f5423a396f6e/inf/c5HgGNO3Fbj4aGCb6PUSiOEbIOlotRaOEQavkx5U5lxlArNUxfSXoKt26msCI5v0LeNEbbhCPkgsCYdatAlHwA%3D%3D?uid=1648264466&filename=dombay.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1648264466&tknv=v2&size=2560x1232'
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-    name: 'Красная поляна',
-    link: 'https://1.downloader.disk.yandex.ru/preview/bcb2ffe09cf979259c610a50c9a0ea5d62e101d8c75aeace2a43f53eabda451d/inf/TsmurVyB1-Xt_W2qTBC___tIJdm75P8EqnlgoM4RGwVx9YJ-fDYGGdK-_igAZiXsJuwrLgP1sMyly79OjOhTeQ%3D%3D?uid=1648264466&filename=b892cf583fbdbc94ac32284994144fde.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1648264466&tknv=v2&size=2560x1232'
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
